af203b363487f7d0f50895b46b6f637587f337b6
P.Q <P.Q@DESKTOP-9E02DLV>
2023-03-24 10:06:10 +0700

    upload source code

---
+++ js/contact-collapsible.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#accordion, #bs-collapse')
+    .on('show.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function(a) {
+      $(a.target).prev('.panel-heading').removeClass('active');
+    });
+});
